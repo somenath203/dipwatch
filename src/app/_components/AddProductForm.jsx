7afb459745fd47f7293c30eca063332957040126
Somenath Choudhury <somenathchoudhury35@gmail.com>
2026-08-29 22:22:35 +0530

    created the landing page

---
+++ src/app/_components/AddProductForm.jsx
@@ -0,0 +1,80 @@
+"use client";
+
+import { useState } from "react";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
+
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+
+
+const AddProductForm = ({ user }) => {
+
+
+  const [urlInput, setUrlInput] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+
+  const handleSubmit = async (e) => {
+
+    e.preventDefault();
+
+    try {
+
+      setLoading(true);
+
+    } catch (error) {
+
+      console.log(error);
+
+      toast.error("Something went wrong. Please try again after sometime.");
+
+    } finally {
+
+      setLoading(false);
+
+    }
+
+  };
+
+  return (
+    <>
+      <form className="w-full max-w-2xl mx-auto" onSubmit={handleSubmit}>
+
+        <div className="flex flex-col sm:flex-row gap-2">
+
+          <Input
+            type="url"
+            placeholder="paste product URL (Amazon, Walmart, etc.)"
+            value={urlInput}
+            onChange={(e) => setUrlInput(e.target.value)}
+            className="h-12 text-base border-2"
+            required
+            disabled={loading}
+          />
+
+          <Button
+            type="submit"
+            disabled={loading}
+            className="bg-orange-500 hover:bg-orange-600 h-10 sm:h-12 px-8 hover:cursor-pointer"
+            size="lg"
+          >
+            {loading ? (
+              <Loader2 className="mr-2 size-4 animate-spin duration-150" />
+            ) : (
+              "Track Price"
+            )}
+          </Button>
+
+        </div>
+
+      </form>
+
+      {/* authentication modal */}
+      
+    </>
+  );
+};
+
+export default AddProductForm;
---
+++ src/app/layout.js
@@ -0,0 +1,38 @@
+import { Geist, Geist_Mono } from "next/font/google";
+import { Toaster } from "sonner";
+
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "EcomDealScrap",
+  description: "Track prices from any e-commerce site.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+    >
+      <body className="min-h-full flex flex-col" suppressHydrationWarning>
+
+        {children}
+
+        <Toaster position="top-right" richColors={true} />
+
+      </body>
+      
+    </html>
+  );
+}
